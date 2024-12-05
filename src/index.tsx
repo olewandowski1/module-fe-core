8285574a36d7812383e44070b4a75ab92a1a9f19
olewandowski1 <olewandowski@soldevelo.com>
2024-12-05 21:06:50 +0100

    build: add zustand as a peer, fix index

--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,12 @@ import { App } from '@/components/app';
 import { Button } from '@/components/button';
 import { cn } from '@/lib/utils';
 
+// Main entry point for the module, include all configurations here
+const CORE_MODULE_CONFIG = {
+  contributionExample: ['App', 'Button'],
+};
+
+// Export all components and utils to be used by other modules
 export { App, Button, cn };
+
+export default CORE_MODULE_CONFIG;
