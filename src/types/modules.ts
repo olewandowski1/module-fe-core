77b179ba83892a9aac11fe4f5916e02f263fb2be
olewandowski1 <olewandowski@soldevelo.com>
2024-12-13 22:45:18 +0100

    fix: remove unused eslint-disable

--- src/types/modules.ts
+++ src/types/modules.ts
@@ -2,13 +2,11 @@ import { JSX } from 'react';
 
 export type Module = {
   name: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   entry: Record<string, any>;
 };
 
 export type Ref = {
   key: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   value: React.FC<any> | string | number | boolean;
 };
 
